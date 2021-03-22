a9ba2d86e2255ed2a244cbb7b629c739f09610b3
Marcelo J. Both <marcelo.both@gmail.com>
2021-03-22 10:23:47 -0300

    doc: added explanation about decisions and architecture.

--- app/adapters/http_request.rb
+++ app/adapters/http_request.rb
@@ -1,13 +1,16 @@
 require 'faraday'
 
+# This class wraps Faraday class.
+#
+# The idea of doing this is to avoid to bring Faraday to the core logic and create a dependencie on the class
+# By having this class if Faraday it's not a good choice anymore it can be replaced without create a big change.
 class HttpRequest
   attr_reader :conn, :responses
 
   def initialize(url:)
     @conn = Faraday.new(url: url) do |builder|
       builder.request :retry, max: MAX_RETRIES, interval: 0.05,
-        interval_randomness: 0.5, backoff_factor: 2,
-        exceptions: [ResponseError, 'Timeout::Error']
+        interval_randomness: 0.5, backoff_factor: 2
 
       builder.request  :url_encoded
       builder.response :logger
--- app/services/social_network_service.rb
+++ app/services/social_network_service.rb
@@ -1,21 +1,31 @@
 class SocialNetworkService
   attr_accessor :parsed_response
 
-  def initialize(http_concurrent)
-    @http_concurrent = http_concurrent
+  def initialize(http_request)
+    # HTTP Request class is injected to this class
+    #
+    # By doing so, it not depend directly to HttpRequest class,
+    # the only thing that metters is that the injected object responds to the expected methods:
+    # - run_in_parallel
+    # - run_single_request
+    #
+    # This will allow to change or to use other implementations of a "http request" without adding more complexity.
+    @http_request = http_request
     @parsed_response = {}
   end
 
   def call
-    responses = @http_concurrent.run_in_parallel(endpoints: TAKEHOME_ENDPOINTS)
+    responses = @http_request.run_in_parallel(endpoints: TAKEHOME_ENDPOINTS)
     responses = validate_responses(responses)
     format_response(responses)
   end
 
   private
 
+  # this method will validate and retry a request when something went wrong.
+  # The number of times to retry is defined on MAX_RETRIES constant.
   def validate_responses(responses)
-    failed_responses = responses.select { |response| response[:data].status == 500 }
+    failed_responses = responses.select { |response| !response[:data].success? }
 
     failed_responses.each do |response|
       new_response = retry_response(response)
@@ -27,14 +37,15 @@ class SocialNetworkService
   end
 
   def retry_response(response)
-    retried_response = @http_concurrent.run_single_request(endpoint: response[:endpoint])
+    retried_response = @http_request.run_single_request(endpoint: response[:endpoint])
 
-    raise ResponseError if retried_response[:data].status == 500
+    raise ResponseError unless retried_response[:data].success?
 
     retried_response
   rescue ResponseError
     retries ||= 1
-    return [] if retries == MAX_RETRIES
+    Rails.logger.info "retries for #{response[:endpoint]}: #{retries}"
+    return retried_response if retries == MAX_RETRIES
     retries += 1
     retry
   end
@@ -48,7 +59,7 @@ class SocialNetworkService
   end
 
   def parse_response(endpoint:, response:)
-    return @parsed_response.store(endpoint.to_sym, []) unless response.status == 200
+    return @parsed_response.store(endpoint.to_sym, []) unless response.success?
 
     @parsed_response.store(endpoint.to_sym, JSON.parse(response.body, symbolize_names: true))
   end
